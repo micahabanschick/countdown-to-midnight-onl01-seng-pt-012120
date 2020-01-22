8324a93c62018a8d95fb91a5679cdcdde50c732e
Micah Banschick <micha.ban@gmail.com>
2020-01-22 14:25:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(number)
   end 
   return "HAPPY NEW YEAR!"
 end
+
+def countdown(number)
+  loop do 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    break if number == 0 
+  end 
+  return "HAPPY NEW YEAR!"
+end
