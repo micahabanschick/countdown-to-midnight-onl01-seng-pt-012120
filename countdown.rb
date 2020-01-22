dbd410eff275e4941f9d6fab701b0e8f308f9a59
Micah Banschick <micha.ban@gmail.com>
2020-01-22 14:32:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,10 +9,10 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  while number >= 0 
-    puts "#{number} SECOND(S)!"
-    number -= 1 
+def countdown_with_sleep(seconds)
+  while seconds >= 0 
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1 
   end 
   return "HAPPY NEW YEAR!"
 end
